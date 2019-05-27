0c938aa0dc05c783a3d88a7e4f0f3724376c38ef
tony <anthony.k.franks@gmail.com>
2019-05-27 00:24:28 -0500

    first commit. setting up iot demo and heroku

---
+++ client.js
@@ -0,0 +1,21 @@
+const url = 'ws://akf-iot.herokuapp.com:8080'
+const WebSocket = require('ws')
+const connection = new WebSocket(url)
+
+connection.onopen = () => {
+  connection.send('connected##') 
+}
+
+connection.onerror = (error) => {
+  console.log(`WebSocket error: ${error}`)
+}
+
+connection.onmessage = (e) => {
+  console.log(e.data)
+    if(e.data === "ack##") {
+        console.log("Connection Acknowledged!")
+    }
+    else if(e.data === "what##") {
+        console.log("Unknown Message Sent From Client"); 
+    }
+}
